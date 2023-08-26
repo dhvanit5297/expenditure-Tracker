e67bb26e3e0d1bc0159ca4f7fccf723971f3b096
Dhvanit <dhvanitkubavat5297@gmail.com>
2023-08-26 08:28:18 +0530

    styling !st Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import './ExpenseItem.css'
 
 function ExpenseItem() {
   return (
-    <h1>ExpenseItem</h1>
-  )
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
