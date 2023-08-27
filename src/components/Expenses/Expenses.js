0a8e7dc203cbbf2dae46bb892e211840af4f3566
Dhvanit <dhvanitkubavat5297@gmail.com>
2023-08-27 12:50:24 +0530

    Working with Event and States

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,42 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem'
-import './Expenses.css'
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
+
+  const filteredValue = (filterData)=>{
+      console.log(filterData)
+  }
+
   return (
-    <div className='expenses'>
-        <ExpenseItem
+    <div className="expenses">
+      <ExpensesFilter onFilteredValue= {filteredValue}></ExpensesFilter>
+      <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
       ></ExpenseItem>
 
-<ExpenseItem
+      <ExpenseItem
         title={props.items[1].title}
         amount={props.items[1].amount}
         date={props.items[1].date}
       ></ExpenseItem>
 
-<ExpenseItem
+      <ExpenseItem
         title={props.items[2].title}
         amount={props.items[2].amount}
         date={props.items[2].date}
       ></ExpenseItem>
 
-<ExpenseItem
+      <ExpenseItem
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
       ></ExpenseItem>
     </div>
-  )
+  );
 }
 
-export default Expenses
+export default Expenses;
