4254eac9ce8ac38db9f4afd713338b0611ba5586
Dhvanit <dhvanitkubavat5297@gmail.com>
2023-08-27 19:51:50 +0530

    render data using Map method

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,9 @@
-import React,{useState} from "react";
+import React from "react";
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const [title,setTitle] = useState(props.title)
-
-  const clickHandler = ()=>{
-    setTitle("updated!")
-  }
+  
 
 
 
@@ -16,10 +12,9 @@ function ExpenseItem(props) {
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>click here</button>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,37 +4,20 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
-
-  const filteredValue = (filterData)=>{
-      console.log(filterData)
-  }
+  const filteredValue = (filterData) => {
+    console.log(filterData);
+  };
 
   return (
     <div className="expenses">
-      <ExpensesFilter onFilteredValue= {filteredValue}></ExpensesFilter>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
+      <ExpensesFilter onFilteredValue={filteredValue}></ExpensesFilter>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        ></ExpenseItem>
+      ))}
     </div>
   );
 }
